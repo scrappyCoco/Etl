a988d8136e05062ef91cb5e62d6b7ef03487b77c
Artem <korshunovartem@mail.ru>
2025-09-20 15:13:38 +0300

    Sed replaced.

--- Build.sh
+++ Build.sh
@@ -4,10 +4,10 @@ semVer=`dotnet gitversion /showvariable SemVer`
 dotnet build ./src/EtlLauncher/EtlLauncher.csproj --configuration Release
 dotnet build ./test/TestData/StageDb/StageDb.sqlproj --configuration Release
 
-$sourceVersionPattern="Include=\"Coding4Fun.Sql.Build\"\\s+Version=\"1.0.0\""
-$replacementPattern="Include=\"Coding4Fun.Sql.Build\" Version=\"${semVer}\""
+sourceVersionPattern="Include=\"Coding4Fun.Sql.Build\"\\s+Version=\"1.0.0\""
+replacementPattern="Include=\"Coding4Fun.Sql.Build\" Version=\"${semVer}\""
 $sedCommand="s/${sourceVersionPattern}/${replacementPattern}/"
-sed -i '' $sedCommand "Directory.Packages.props"
+sed -i '' "${sedCommand}" "Directory.Packages.props"
 
 dotnet restore ./test/TestData/CoreDb/CoreDb.sqlproj --configfile ./test/TestData/CoreDb/nuget.config
 dotnet build ./test/TestData/CoreDb/CoreDb.sqlproj --configuration Release --no-restore
