d00d5955932a117306d58f74bd49ada673546c71
Artem <korshunovartem@mail.ru>
2025-09-20 15:22:28 +0300

    Another one change with sed.

--- Build.sh
+++ Build.sh
@@ -7,7 +7,7 @@ dotnet build ./test/TestData/StageDb/StageDb.sqlproj --configuration Release
 sourceVersionPattern="Include=\"Coding4Fun.Sql.Build\"\\s+Version=\"1.0.0\""
 replacementPattern="Include=\"Coding4Fun.Sql.Build\" Version=\"${semVer}\""
 $sedCommand="s/${sourceVersionPattern}/${replacementPattern}/"
-sed -i '' "${sedCommand}" "Directory.Packages.props"
+sed "${sedCommand}" "Directory.Packages.props"
 
 dotnet restore ./test/TestData/CoreDb/CoreDb.sqlproj --configfile ./test/TestData/CoreDb/nuget.config
 dotnet build ./test/TestData/CoreDb/CoreDb.sqlproj --configuration Release --no-restore
