2ed57f319b292c0173a174af264e8879e5b1f2b0
Artem <korshunovartem@mail.ru>
2025-09-20 14:35:26 +0300

    Package version has been updated.

--- Build.ps1
+++ Build.ps1
@@ -3,7 +3,6 @@ dotnet build ./src/Build/Build.csproj --configuration Release
 $semVer=dotnet gitversion /showvariable SemVer
 dotnet build ./src/EtlLauncher/EtlLauncher.csproj --configuration Release
 dotnet build ./test/TestData/StageDb/StageDb.sqlproj --configuration Release
-dotnet add package Coding4Fun.Sql.Build --version $semVer --force-version-update
 dotnet restore ./test/TestData/CoreDb/CoreDb.sqlproj --configfile ./test/TestData/CoreDb/nuget.config
 dotnet build ./test/TestData/CoreDb/CoreDb.sqlproj --configuration Release --no-restore
 dotnet test ./test/BuildTest/BuildTest.csproj
