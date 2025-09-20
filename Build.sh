5928c13741cc3b94c711d85a684da4facd7afece
Artem <korshunovartem@mail.ru>
2025-09-20 14:15:17 +0300

    SemVer has been added.

--- Build.sh
+++ Build.sh
@@ -1,8 +1,9 @@
 dotnet tool restore
 dotnet build ./src/Build/Build.csproj --configuration Release
-dotnet gitversion
+semVer=`dotnet gitversion /showvariable SemVer`
 dotnet build ./src/EtlLauncher/EtlLauncher.csproj --configuration Release
 dotnet build ./test/TestData/StageDb/StageDb.sqlproj --configuration Release
+dotnet add package Coding4Fun.Sql.Build --version $semVer --force-version-update
 dotnet restore ./test/TestData/CoreDb/CoreDb.sqlproj --configfile ./test/TestData/CoreDb/nuget.config
 dotnet build ./test/TestData/CoreDb/CoreDb.sqlproj --configuration Release --no-restore
 dotnet test ./test/BuildTest/BuildTest.csproj
