eeb313f9ad75b912b53f11a63007a31e1fb04723
coding4fun <korshunovartem@mail.ru>
2025-09-21 10:59:40 +0300

    Feature/build (#1)
    * SemVer has been added.

--- Build.sh
+++ Build.sh
@@ -1,7 +1,25 @@
+# Output all commands
+set -o xtrace
+
+dotnet tool restore
 dotnet build ./src/Build/Build.csproj --configuration Release
+semVer=`dotnet gitversion /showvariable SemVer`
 dotnet build ./src/EtlLauncher/EtlLauncher.csproj --configuration Release
 dotnet build ./test/TestData/StageDb/StageDb.sqlproj --configuration Release
+
+sourceVersionPattern="1.0.0"
+replacementPattern="${semVer}"
+sedCommand="s/${sourceVersionPattern}/${replacementPattern}/g"
+sed -i "${sedCommand}" "Directory.Packages.props"
+
 dotnet restore ./test/TestData/CoreDb/CoreDb.sqlproj --configfile ./test/TestData/CoreDb/nuget.config
 dotnet build ./test/TestData/CoreDb/CoreDb.sqlproj --configuration Release --no-restore
 dotnet test ./test/BuildTest/BuildTest.csproj
 dotnet test ./test/IntegrationTest/IntegrationTest.csproj
+
+branchName=`git rev-parse --abbrev-ref HEAD`
+
+if [ "$branchName" = "main" ]
+then
+    echo "${semVer}"
+fi
