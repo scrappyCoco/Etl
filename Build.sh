fb9516ee1cd975d69522e2c0bbbc4e4c3dd9195a
Artem <korshunovartem@mail.ru>
2025-09-21 12:37:54 +0300

    nuget push has been added.

--- Build.sh
+++ Build.sh
@@ -5,3 +5,12 @@ dotnet restore ./test/TestData/CoreDb/CoreDb.sqlproj --configfile ./test/TestDat
 dotnet build ./test/TestData/CoreDb/CoreDb.sqlproj --configuration Release --no-restore
 dotnet test ./test/BuildTest/BuildTest.csproj
 dotnet test ./test/IntegrationTest/IntegrationTest.csproj
+
+branchName=`git rev-parse --abbrev-ref HEAD`
+
+if [ "$branchName" = "main" ]
+then
+    apiKey=`printenv NUGET_API_KEY`
+    echo "${semVer}"
+    dotnet nuget push ./src/Build/Build/bin/Release/Coding4Fun.Sql.Build.$semVer.nupkg -k $apiKey -s https://api.nuget.org/v3/index.json
+fi
