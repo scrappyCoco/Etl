8490e369d4c642c5adde7acdeb1a7052339c799f
Artem <korshunovartem@mail.ru>
2025-10-04 17:46:11 +0300

    Packaging of Launcher has been reworked.

--- Build.sh
+++ Build.sh
@@ -4,7 +4,13 @@ set -o xtrace
 dotnet tool restore
 dotnet build ./src/Build/Build.csproj --configuration Release
 semVer=`dotnet gitversion /showvariable SemVer`
-dotnet build ./src/Launcher/Launcher.csproj --configuration Release
+
+# Building Launcher
+dotnet build ./src/Launcher/Launcher.csproj --configuration Release --runtime win-x64
+dotnet build ./src/Launcher/Launcher.csproj --configuration Release --runtime linux-x64
+dotnet build ./src/Launcher/Launcher.csproj --configuration Release --runtime osx-x64
+dotnet pack ./src/Launcher/Launcher.csproj --configuration Release
+
 dotnet build ./test/TestData/StageDb/StageDb.sqlproj --configuration Release
 
 sourceVersionPattern="1.0.0"
@@ -18,10 +24,11 @@ dotnet test ./test/BuildTest/BuildTest.csproj
 dotnet test ./test/IntegrationTest/IntegrationTest.csproj
 
 branchName=`git rev-parse --abbrev-ref HEAD`
+tagWithVersion=`git tag --points-at HEAD | grep -E 'v\d+\.\d+\.\d+'`
 
-if [ "$branchName" = "main" ]
+echo "${semVer}"
+if [ "$branchName" = "main" ] && [ -n "$tagWithVersion" ]
 then
-    echo "${semVer}"
     dotnet nuget push ./src/Build/bin/Release/Coding4Fun.Etl.Build.$semVer.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
     dotnet nuget push ./src/Launcher/bin/Release/Coding4Fun.Etl.Launcher.$semVer.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
 fi
