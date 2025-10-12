4c9e826290f92119ee9d446bf5b0cacc1c2de905
Artem <korshunovartem@mail.ru>
2025-10-12 20:52:32 +0300

    Support for new lines in Windows has been added for BatchColumnMarkerRegex.

--- src/Build/BuildEltTask.cs
+++ src/Build/BuildEltTask.cs
@@ -37,7 +37,7 @@ public class BuildEltTask : Microsoft.Build.Utilities.Task
     /// <summary>
     /// Regex pattern to identify batch column markers in SQL code.
     /// </summary>
-    private readonly Regex BatchColumnMarkerRegex = new("C4F[.]ETL[.]BatchColumns:\\s*(?<value>[^\n]+)", RegexOptions.Compiled);
+    private readonly Regex BatchColumnMarkerRegex = new("C4F[.]ETL[.]BatchColumns:\\s*(?<value>[^\r\n]+)", RegexOptions.Compiled);
 
     /// <summary>
     /// Path to the source DACPAC file containing database schema definitions.
